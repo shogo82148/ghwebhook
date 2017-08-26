e3af4e58321556fb584c609fc4964003af9e9dab
Ichinose Shogo <shogo82148@gmail.com>
2017-08-26 14:33:29 +0900

    add ping handler

---
+++ _example/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/google/go-github/github"
+	"github.com/shogo82148/ghwebhook"
+)
+
+func main() {
+	h := &ghwebhook.Webhook{
+		Ping: func(e *github.PingEvent) {
+			log.Printf("%#v", e)
+		},
+	}
+	http.ListenAndServe(":8080", h)
+}
---
+++ ghwebhook.go
@@ -0,0 +1,55 @@
+package ghwebhook
+
+import (
+	"io/ioutil"
+	"mime"
+	"net/http"
+
+	"github.com/google/go-github/github"
+)
+
+type Webhook struct {
+	Ping func(e *github.PingEvent)
+}
+
+func (h *Webhook) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+
+	t, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+	var payload []byte
+	switch t {
+	case "application/x-www-form-urlencoded":
+		payload = []byte(r.PostFormValue("payload"))
+	case "application/json":
+		payload, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	default:
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	e, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	go h.handle(e)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Webhook) handle(e interface{}) {
+	switch e := e.(type) {
+	case *github.PingEvent:
+		if h.Ping != nil {
+			h.Ping(e)
+		}
+	}
+}
